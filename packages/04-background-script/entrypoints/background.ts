e3a59e48055f61f4dd02773b6f54dc605c79cb25
rxliuli <rxliuli@gmail.com>
2026-01-16 16:31:26 +0800

    feat: add 04-background-script

--- packages/04-background-script/entrypoints/background.ts
+++ packages/04-background-script/entrypoints/background.ts
@@ -1,3 +1,38 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
-});
+  console.log('Hello background!', { id: browser.runtime.id })
+
+  const lastAcessed = new Map<number, number>()
+
+  browser.tabs.onCreated.addListener((tab) => {
+    if (!tab.id) {
+      return
+    }
+    lastAcessed.set(tab.id, Date.now())
+  })
+  browser.tabs.onRemoved.addListener((tabId) => {
+    lastAcessed.delete(tabId)
+  })
+  browser.tabs.onActivated.addListener((activeInfo) => {
+    lastAcessed.set(activeInfo.tabId, Date.now())
+    console.log('Tab activated:', activeInfo.tabId)
+    autoDiscardTabs()
+  })
+  async function autoDiscardTabs() {
+    const Timeout = 30 * 60 * 1000
+    const tabs = (await browser.tabs.query({})).filter(
+      (tab) =>
+        tab.id &&
+        !tab.pinned &&
+        !tab.active &&
+        !tab.audible &&
+        !tab.frozen &&
+        !tab.discarded &&
+        lastAcessed.has(tab.id) &&
+        Date.now() - lastAcessed.get(tab.id)! > Timeout,
+    )
+    for (const tab of tabs) {
+      await browser.tabs.discard(tab.id!)
+      console.log('Tab auto-discarded:', tab.id, tab.title)
+    }
+  }
+})
--- packages/04-background-script/wxt.config.ts
+++ packages/04-background-script/wxt.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'wxt'
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
   manifestVersion: 3,
   manifest: {
     name: 'Auto Sleep Tabs',
     description:
       'Automatically puts inactive tabs to sleep to save memory and CPU.',
+    permissions: ['tabs'],
   },
   webExt: {
     disabled: true,
